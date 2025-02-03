a047e0af4b508296b4292c17313f217384a2513b
unknown <rxtmark@gmail.com>
2025-02-03 14:43:59 +0200

    oauth fix

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import passport from "passport";
 import authRoutes from "./routes/authRoutes.js";
-const passportSetup = require("./config/passport.js");
+import passport from "./config/passport.js"
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(passportSetup.initialize());
+app.use(passport.initialize());
 
 app.use("/api/auth", authRoutes);
 
