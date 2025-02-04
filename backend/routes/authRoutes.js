0255b1aaf71650423ac1b9cc59baf386269708dc
unknown <rxtmark@gmail.com>
2025-02-04 16:44:22 +0200

    Oauth google fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -18,6 +18,16 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/set-password", protect, setPassword);
 
+router.get("/google", passport.authenticate("google"));
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  (req, res) => {
+    if (!req.user) return handleError(res, "Authentication failed", 400);
+    res.status(200).json({ user: req.user, token: generateToken(req.user) });
+  }
+);
+
 router.get("/github", passport.authenticate("github"));
 router.get(
   "/github/callback",
