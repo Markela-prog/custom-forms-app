a702ac7c859615f34727318df30e7dfc1204c072
unknown <rxtmark@gmail.com>
2025-02-17 21:40:51 +0200

    access control refactor

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.put("/reorder", protect, accessControl("question", "reorder"), reorderQuestionsController);
 router.get("/:templateId", optionalAuth, accessControl("template", "read"), getQuestionsByTemplateController);
-router.post("/:templateId", protect, accessControl("template", "create"), createQuestionController);
+router.post("/:templateId", protect, accessControl("question", "create"), createQuestionController);
 router.put("/:questionId", protect, accessControl("question", "update"), updateQuestionController);
 router.delete("/:questionId", protect, accessControl("question", "delete"), deleteQuestionController);
 
