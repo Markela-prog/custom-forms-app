1a69a52b23cd49e61bce11a484d31249a5876dc0
unknown <rxtmark@gmail.com>
2025-02-18 15:24:51 +0200

    access control refactor

--- backend/middleware/answerAccessMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const checkAnswerAccess = async (req, res, next) => {
-  const user = req.user;
-
-  if (user.role !== "ADMIN") {
-    return res.status(403).json({ message: "Only admin can manage answers" });
-  }
-
-  next();
-};
