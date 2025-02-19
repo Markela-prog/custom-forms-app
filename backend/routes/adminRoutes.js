30cd503c1ddb9381e2bcca8c6d29a17005c40224
unknown <rxtmark@gmail.com>
2025-02-19 14:28:04 +0200

    access control refactor

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,17 +2,17 @@ import express from "express";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 import {
   getAllUsers,
-  promoteUser,
-  demoteUser,
-  deleteUser,
+  promoteUsers,
+  deleteUsers,
+  demoteUsers
 } from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.get("/users", protect, isAdmin, getAllUsers);
-router.post("/promote", protect, isAdmin, promoteUser);
-router.post("/demote", protect, isAdmin, demoteUser);
-router.delete("/delete", protect, isAdmin, deleteUser);
+router.post("/promote", protect, isAdmin, promoteUsers);
+router.post("/demote", protect, isAdmin, demoteUsers);
+router.delete("/delete", protect, isAdmin, deleteUsers);
 
 export default router;
 
